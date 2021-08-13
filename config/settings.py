ac09f3ebe59d4286951e0b131f7e30ebae376367
Ryuta44 <56989524+Ryuta44@users.noreply.github.com>
2021-08-13 11:18:36 +0900

    アカウント作成
    ・accountsアプリの作成
    ・メール認証による新規アカウント作成<-現在はターミナルにメールが送信される
    ・カスタムユーザーモデルの作成

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recpos'
+    'recpos',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# ユーザー認証用に追加
+
+AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'accounts:top'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
