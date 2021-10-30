f639c1e671b865a2c8c69457bd41a813f2365972
Yukihiro Kozai <50065338+Yukhy@users.noreply.github.com>
2021-10-30 15:41:11 +0900

    marge branch of create-accounts into main

--- recpos/views.py
+++ recpos/views.py
@@ -52,6 +52,3 @@ def mailbox(request):
     service = gmail_get_service(request.user)
     msg = gmail_get_messages(service)
     return render(request, 'recpos/mailbox.html')
-
-def login(request):
-    return render(request, 'recpos/tmpLogin.html')
