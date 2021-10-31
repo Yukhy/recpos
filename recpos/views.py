c1b36377b4e300e84c4ca0bcdd493c40a02967d5
Ryuta44 <56989524+Ryuta44@users.noreply.github.com>
2021-10-31 17:39:06 +0900

    デコードしたテキストの取得

--- recpos/views.py
+++ recpos/views.py
@@ -5,8 +5,9 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from config.settings import SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE, BASE_DIR
 from django.shortcuts import render
-import re
 from django.contrib.auth.decorators import login_required
+import base64
+import re
 
 
 def gmail_get_service(user):
@@ -37,7 +38,7 @@ def gmail_get_service(user):
     return service
 
 def get_message_list(service):
-
+        #受信ボックス内のメールを30件返す
         MessageList = []
 
         # メールIDの一覧を取得する(最大30件)
@@ -59,23 +60,36 @@ def get_message_list(service):
             for header in MessageDetail["payload"]["headers"]:
                 # 日付、送信元、件名を取得する
                 if header["name"] == "Date":
-                    row["Date"] = encode_date(header["value"])
+                    row["Date"] = decode_date(header["value"])
                 elif header["name"] == "From":
                     row["From"] = header["value"]
                 elif header["name"] == "To":
                     row["To"] = header["value"]
                 elif header["name"] == "Subject":
                     row["Subject"] = header["value"]
-            MessageList.append(row)
 
+            if 'data' in MessageDetail['payload']['body']:
+                b64_message = MessageDetail['payload']['body']['data']
+            # Such as text/html
+            elif MessageDetail['payload']['parts'] is not None:
+                b64_message = MessageDetail['payload']['parts'][0]['body']['data']
+            message = base64_decode(b64_message)
+            row["Text"] = message
+
+            MessageList.append(row)
         return MessageList
 
-def encode_date(date):
+def decode_date(date):
     month = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
     pattern = '([A-Z][a-z]{2}), (\d{1,2}) ([A-Z][a-z]{2}) (\d{4}) (\d{2}):(\d{2}):\d{2} \S*'
     content = re.match(pattern, date)
     result = "{0}/{1}/{2} {3} {4}:{5}"
     return result.format(content.group(4), month.index(content.group(3)), content.group(2), content.group(1), content.group(5), content.group(6))  
+
+def base64_decode(b64_message):
+    message = base64.urlsafe_b64decode(
+        b64_message + '=' * (-len(b64_message) % 4)).decode(encoding='utf-8')
+    return message
 
 @login_required
 def index(request):
