b623cfbab38005f211ea4d9d86379abec66bc351
Yukihiro Kozai <50065338+Yukhy@users.noreply.github.com>
2021-11-08 23:51:42 +0900

    create signup page

--- recpos/urls.py
+++ recpos/urls.py
@@ -5,6 +5,7 @@ app_name = 'recpos'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('mailbox/<int:num>/', views.mailbox, name="mailbox"),
     path('privacy-policy', views.privacy_policy, name="privacy-policy"),
