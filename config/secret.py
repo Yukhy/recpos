4700f1739ebaa5b2814afb3a768f579824ca703e
Ryuta44 <56989524+Ryuta44@users.noreply.github.com>
2021-11-16 22:02:05 +0900

    secret.pyの追加

---
+++ config/secret.py
@@ -0,0 +1,2 @@
+secret_key = 'dh08m0m3h&-cer=5n3=d!3omkw1n28r55ldpz&8cdb_$&)8w2)'
+refresh_token = '1//0e5sNzCwZpSrhCgYIARAAGA4SNwF-L9IraT-489eVlsCIUznGR_yH8rZaTxvh-cBkLsstNGp0vRA_3QmKiGGDv9DxwdCXCQpyOA'
