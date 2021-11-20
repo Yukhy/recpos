f2402dbf509dfb0c0869dd01109cf9defa0e5b58
Ryuta44 <56989524+Ryuta44@users.noreply.github.com>
2021-11-20 22:58:51 +0900

    軽微な修正

--- config/secret.py
+++
@@ -1,2 +0,0 @@
-secret_key = 'dh08m0m3h&-cer=5n3=d!3omkw1n28r55ldpz&8cdb_$&)8w2)'
-refresh_token = '1//0e5sNzCwZpSrhCgYIARAAGA4SNwF-L9IraT-489eVlsCIUznGR_yH8rZaTxvh-cBkLsstNGp0vRA_3QmKiGGDv9DxwdCXCQpyOA'
